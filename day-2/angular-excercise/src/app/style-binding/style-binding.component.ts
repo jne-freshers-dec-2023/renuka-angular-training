b11c42628e2f91e64f3642300b5c643948d38966
renukaThink <renuka.pogul@thinkitive.com>
2024-01-23 19:40:50 +0530

    Style Binding

---
+++ day-2/angular-excercise/src/app/style-binding/style-binding.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-style-binding',
+  templateUrl: './style-binding.component.html',
+  styleUrls: ['./style-binding.component.css']
+})
+export class StyleBindingComponent {
+  bgcolor="red";
+  changeColor(colour:string){
+    this.bgcolor=colour
+  }
+}
