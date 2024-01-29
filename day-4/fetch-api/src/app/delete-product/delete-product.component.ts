54696e745312a7fad0ec8a290a30b31776406396
renukaThink <renuka.pogul@thinkitive.com>
2024-01-29 19:37:46 +0530

    Delete product with id

---
+++ day-4/fetch-api/src/app/delete-product/delete-product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-product',
+  templateUrl: './delete-product.component.html',
+  styleUrls: ['./delete-product.component.css']
+})
+export class DeleteProductComponent {
+
+  constructor(){
+    console.log('delete product')
+  }
+}
